e17344890db0df765e188edd8d286065522f9cc8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-11 19:56:37 +0000

    Deploy utensils/coinulator to github.com/utensils/coinulator.git:gh-pages

---
+++ precache-manifest.320c2267b130d7f8f23359c4132a92af.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d594c5e5aa34b6a9d39",
+    "url": "https://utensils.io/coinulator/static/js/main.7d594c5e.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "https://utensils.io/coinulator/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "5a8e50b7cbf3f62ead9e",
+    "url": "https://utensils.io/coinulator/static/js/runtime~main.5a8e50b7.js"
+  },
+  {
+    "revision": "26a4dd253fcb0eb5d626db5ec5de62ec",
+    "url": "https://utensils.io/coinulator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/coinulator/precache-manifest.ef6a137ea848e5c232d06afaa084b19f.js"
+  "https://utensils.io/coinulator/precache-manifest.320c2267b130d7f8f23359c4132a92af.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/coinulator/index.html", {
+workbox.routing.registerNavigationRoute("https://utensils.io/coinulator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
